5476f01de036f36e893ce4e39ae3af882e25a4b0
Shubham Kumar <shubhamkashyap9501@gmail.com>
2026-01-23 12:05:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 pip install -r requirements.txt
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
-playwright install chromium
+playwright install chromium --with-deps || playwright install chromium
